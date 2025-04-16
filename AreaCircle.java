4475f661a923c406cde87c3d9f0c37f833751d0d
ananyasamadhiy1 <ananyasamadhiya2004@gmail.com>
2025-04-16 13:29:10 +0530

    Add second code project

--- AreaCircle.java
+++ AreaCircle.java
@@ -1,17 +1,12 @@
 import java.util.Scanner;
 
 
-class AreaCircle{
+class Pattern{
     public static void main(String[] args)
 {
-    Scanner sc = new Scanner (System.in);
-    int s = sc.nextInt("enter a no.");
-   
-    if(s%2==0 ){
-        System.out.println(s+"is even no.");
-}
-    else{System.out.println(s+"is odd no.");
-}   
-    sc.close();
-
+    for (int line= 1; line<=4; line++);
+    {
+        for(int i = line; i <= line; i++);
+        System.out.print("*");
+    }
 }}
