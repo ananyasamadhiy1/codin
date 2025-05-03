ddfb1d312bf9d3c1a7d889364d30fab8f0aaa232
ananyasamadhiy1 <127299506+ananyasamadhiy1@users.noreply.github.com>
2025-05-03 23:06:15 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class LinearSearch {
+    public static int search( int marks[], int key){
+        for (int i=0; i<marks.length;i++){
+           if (marks[i]== key){   
+            return i;
+        }}
+        return -1;
+
+    }
+    public static void main(String[] args) {
+        int marks[] = {55,67,99};
+        int key = 67;  
+        int result = search(marks,key);
+        if (result != -1) {
+            System.out.println("Key found at index: " + result);}
+            
+        
+    }
+    else{
+    System.out.println("key not found");
+}
+}
